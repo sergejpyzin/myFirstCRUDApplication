4b108426da7cb0c6ac03bbabbdcf3ef9c242a780
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-04-03 16:32:05 +0300

    commit init

---
+++ src/main/java/ru/sergeypyzin/firstcrudproject/FirstCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.sergeypyzin.firstcrudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCrudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCrudProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sergeypyzin/firstcrudproject/FirstCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sergeypyzin.firstcrudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
